b0881eb03537e071ea74bbe872ea7a31aa87c479
Dominic Lee <dominiclee0512@gmail.com>
2017-09-22 21:58:02 +0800

    Add redux basic todos  module and install some redux dependencies

--- react/client/index.js
+++ react/client/index.js
@@ -1,5 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './modules/todos/components/App'
+import reducer from './modules/todos/reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
