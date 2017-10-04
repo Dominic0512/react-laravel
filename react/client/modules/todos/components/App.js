66a90145bd25399a8ee0466fe501f7a2335a0393
Dominic Lee <dominiclee0512@gmail.com>
2017-10-05 00:12:49 +0800

    Add css,sass,less loader for react component styles

--- react/client/modules/todos/components/App.js
+++ react/client/modules/todos/components/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import '../styles/todos.css'
 
 const ToDoApp = () => (
-  <div>
+  <div className="todo-container">
     <AddTodo />
     <VisibleTodoList />
     <Footer />
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -34,6 +34,48 @@ module.exports = {
         test: /\.jsx$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'less-loader'
+          }
+        ],
+        exclude: /node_modules/
       }
     ]
   },
